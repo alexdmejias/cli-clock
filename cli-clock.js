d8d8a5e63c4c2471effa9b8fccde4ef58c8627a8
Alex Mejias <alex.mejias@mlb.com>
2017-07-13 11:10:25 -0400

    version in help will match package.json

--- cli-clock.js
+++ cli-clock.js
@@ -2,12 +2,13 @@
 
 require('source-map-support/register');
 const program = require('commander');
+const package = require('./package.json');
 const Clock = require('./dist');
 
 const list = (val) => val.split(',');
 
 program
-  .version('0.4.0')
+  .version(package.version)
   .option('-b, --background [character]', 'baground character')
   .option('-f, --foreground [character]', 'foreground characters color')
   .option('-c, --colors [item(s)]', 'characters colors for the foreground and background, respectively', list)
